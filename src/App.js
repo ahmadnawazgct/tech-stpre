70c3ca80cbd87704d2519bb224caf43c5aa16c8b
nawaz ahmad <ahmadnawazgct@gmail.com>
2019-12-15 16:48:13 -0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import { FaHome } from 'react-icons/fa';
+import styled from 'styled-components';
 function App() {
   return (
     <div>
-      <h1>Hello from tect store</h1>
+      <h1>Hello from tech store</h1>
+      <FaHome />
+      <Button>Hello styled</Button>
     </div>
   );
 }
+const Button = styled.button`
+color:red;
+background:blue;
+font-size:2rem; `
 
 export default App;
